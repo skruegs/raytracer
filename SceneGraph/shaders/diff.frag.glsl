638fa29cefceeff6ced17cb455635310ff83e9fb
Sarah Krueger <skrueger@seas.upenn.edu>
2014-11-19 02:01:34 -0500

    Update diff.frag.glsl

--- SceneGraph/shaders/diff.frag.glsl
+++ SceneGraph/shaders/diff.frag.glsl
@@ -1,8 +1,5 @@
 #version 130
-// ^ Change this to version 130 if you have compatibility issues
 
-//these are the interpolated values out of the rasterizer, so you can't know
-//their specific values without knowing the vertices that contributed to them
 in vec4 fs_Normal;
 in vec4 fs_LightVector;
 in vec4 fs_Color;
@@ -13,33 +10,7 @@ in vec4 lightColor;
 out vec4 out_Color;
 
 void main()
-{/*
-    // Material base color (before shading)
-    vec4 diffuseColor = fs_Color;
-	vec4 specColor = lightColor;
-
-    // Calculate the diffuse term
-    float diffuseTerm = dot(normalize(fs_Normal), normalize(fs_LightVector));
-    diffuseTerm = clamp(diffuseTerm, 0, 1);
-
-    float ambientTerm = 0.2;
-
-
-	vec3 reflectDir = reflect(-vec3(fs_LightVector), vec3(fs_Normal));
-	vec3 viewDir = normalize(-vertPos);
-	vec3 halfDir = normalize(vec3(fs_LightVector) + viewDir);
-    //float specAngle = max(dot(halfDir, vec3(fs_Normal)), 0.0);
-    float specAngle = max(dot(reflectDir, viewDir), 0.0);
-	float specularTerm = pow(specAngle, 1.0);
-	
-    float lightIntensity = diffuseTerm + ambientTerm + specularTerm;
-    // Compute final shaded color
-    //out_Color = vec4(diffuseColor.rgb * lightIntensity, diffuseColor.a);
-	//out_Color = vec4((diffuseColor.rgb + specColor.rgb) * lightIntensity, diffuseColor.a);
-	out_Color =  vec4(ambientTerm + diffuseTerm * diffuseColor.rgb + specularTerm * specColor.rgb, 1.0f);
-
-	*/
-
+{
 	vec4 diffuseColor = fs_Color;
 	vec4 specColor = lightColor;
 
