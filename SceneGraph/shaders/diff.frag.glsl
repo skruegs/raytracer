08c896f11c176ed57d79152b283d27c3a73e0455
Sarah Krueger <skrueger@seas.upenn.edu>
2014-12-07 01:42:10 -0500

    Update diff.frag.glsl

--- SceneGraph/shaders/diff.frag.glsl
+++ SceneGraph/shaders/diff.frag.glsl
@@ -11,23 +11,36 @@ out vec4 out_Color;
 
 void main()
 {
+	/* // BLINN-PHONG
 	vec4 diffuseColor = fs_Color;
 	vec4 specColor = lightColor;
-
+	
 	vec3 normal = normalize(vec3(fs_Normal));
 	vec3 lightDir = normalize(vec3(fs_LightVector) - vertPos);
 	float lambertian = max(dot(lightDir,normal), 0.0);
 	float specular = 0.0;
-
+	
 	if(lambertian > 0.0) {
 		vec3 viewDir = normalize(-vertPos);
 		vec3 reflectDir = reflect(-(lightDir), (normal));
 		float specAngle = max(dot(reflectDir, viewDir), 0.0);
 		specular = pow(specAngle, 16.0);
 	}
-
+	
 	float lightIntensity = lambertian + 0.2 + specular;
 	out_Color = vec4((diffuseColor.rgb + specColor.rgb) * lightIntensity, diffuseColor.a);
+	*/
+
+
+    vec4 diffuseColor = fs_Color;
+
+    float diffuseTerm = dot(normalize(fs_Normal), normalize(fs_LightVector));
+    diffuseTerm = clamp(diffuseTerm, 0, 1);
+    
+    float ambientTerm = 0.2;
+    float lightIntensity = diffuseTerm + ambientTerm;
+
+    out_Color = vec4(diffuseColor.rgb * lightIntensity, diffuseColor.a);
 }
 
 
