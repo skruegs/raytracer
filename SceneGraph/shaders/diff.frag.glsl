ec5d86cde602c35d5d26a162c367dd31664f353f
Sarah Krueger <skrueger@seas.upenn.edu>
2014-12-26 02:52:58 -0500

    Update diff.frag.glsl

--- SceneGraph/shaders/diff.frag.glsl
+++ SceneGraph/shaders/diff.frag.glsl
@@ -14,28 +14,8 @@ out vec4 out_Color;
 
 void main()
 {
-	/* // BLINN-PHONG
-	vec4 diffuseColor = fs_Color;
-	vec4 specColor = lightColor;
-	
-	vec3 normal = normalize(vec3(fs_Normal));
-	vec3 lightDir = normalize(vec3(fs_LightVector) - vertPos);
-	float lambertian = max(dot(lightDir,normal), 0.0);
-	float specular = 0.0;
-	
-	if(lambertian > 0.0) {
-		vec3 viewDir = normalize(-vertPos);
-		vec3 reflectDir = reflect(-(lightDir), (normal));
-		float specAngle = max(dot(reflectDir, viewDir), 0.0);
-		specular = pow(specAngle, 16.0);
-	}
-	
-	float lightIntensity = lambertian + 0.2 + specular;
-	out_Color = vec4((diffuseColor.rgb + specColor.rgb) * lightIntensity, diffuseColor.a);
-	*/
 
-<<<<<<< HEAD
-	// Material base color (before shading)
+    // Material base color (before shading)
     vec4 diffuseColor = fs_Color;
 
     // Calculate the diffuse term
@@ -48,17 +28,6 @@ void main()
     float lightIntensity = diffuseTerm + ambientTerm;
 
     // Compute final shaded color
-=======
-
-    vec4 diffuseColor = fs_Color;
-
-    float diffuseTerm = dot(normalize(fs_Normal), normalize(fs_LightVector));
-    diffuseTerm = clamp(diffuseTerm, 0, 1);
-    
-    float ambientTerm = 0.2;
-    float lightIntensity = diffuseTerm + ambientTerm;
-
->>>>>>> FETCH_HEAD
     out_Color = vec4(diffuseColor.rgb * lightIntensity, diffuseColor.a);
 }
 
